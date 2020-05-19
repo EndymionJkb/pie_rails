b6f76468fb463784ea230a6c895b0f8d4739cc77
Jeff <endymionjkb@gmail.com>
2020-05-18 23:01:01 -0400

    Replace hard-coding with initalizers; add comments. NB: comments in .js.erb files cause the server to hang!

---
+++ config/initializers/p_tokens.rb
@@ -0,0 +1,2 @@
+# Ideally we would use pTokens.js to select the node
+NODE_ENDPOINT = 'https://nuc-bridge-3.ngrok.io/pbtc-on-eth/get-native-deposit-address/'
---
+++ config/initializers/portis.rb
@@ -0,0 +1,1 @@
+PORTIS_APP_ID = '9d29d798-81ea-45ff-b88b-0bf97fb207be'
